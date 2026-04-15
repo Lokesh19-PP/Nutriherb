8d64487135758a244191909c90adb8ad3c0e2d34
Lokesh Pawar <lokeshpawar1910@gmail.com>
2026-04-15 23:17:49 +0530

    feat: Register models cleanly into the Django Admin dashboard

--- nutriherb/nutrition/admin.py
+++ nutriherb/nutrition/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import HerbOrFood, HealthIssue, Recipe
 
-# Register your models here.
+@admin.register(HerbOrFood)
+class HerbOrFoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    search_fields = ('name', 'category', 'benefits')
+    list_filter = ('category',)
+
+@admin.register(HealthIssue)
+class HealthIssueAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name', 'description')
+    filter_horizontal = ('herbs_or_foods',)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title', 'ingredients')
+    filter_horizontal = ('herbs_or_foods',)
